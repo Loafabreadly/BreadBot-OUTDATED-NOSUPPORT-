f601d79cad7baf53867623236ed2636912cd580e
Brad <birdgeekthree@gmail.com>
2016-03-06 16:31:42 -0600

    Working on InviteUtil and IRC relay

--- src/main/java/com/github/birdgeek/InviteEvent.java
+++ src/main/java/com/github/birdgeek/InviteEvent.java
@@ -1,9 +1,11 @@
 package com.github.birdgeek;
 
+import net.dv8tion.jda.events.Event;
 import net.dv8tion.jda.events.InviteReceivedEvent;
+import net.dv8tion.jda.hooks.EventListener;
 import net.dv8tion.jda.utils.InviteUtil;
 
-public class InviteEvent {
+public class InviteEvent implements EventListener {
 	
 	public void onInviteReceived(InviteReceivedEvent event) {
 		if (isApprovedUser(event.getAuthor().getUsername())) {
@@ -21,5 +23,12 @@ public class InviteEvent {
 			}
 		}
 			return false;
+	}
+
+
+	@Override
+	public void onEvent(Event arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
